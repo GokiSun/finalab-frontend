dd5687d3e69e0e98b20c0411fc1451a2786994dd
WayneWang <1070546617@qq.com>
2018-07-23 11:13:02 +0800

    update this project

---
+++ scripts/main.js
@@ -0,0 +1,69 @@
+$(function() {
+
+	var flag = false;
+
+	function hover() {
+		if (flag) return false;
+		flag = true;
+		$(this).siblings().animate({
+			width: "15%",
+			height: "400px"
+		}, "100ms");
+		$(this).animate({
+			width: "40%",
+			height: "470px"
+		}, "100ms", function() {
+			flag = false;
+		});
+		
+		$(this).siblings().removeClass('fc-fc-list-item-active').addClass('fc-fc-list-item');
+		$(this).removeClass('fc-fc-list-item').addClass('fc-fc-list-item-active');
+	}
+	$(".fc-fc-list>li").hover(
+		hover,
+		function() {});
+
+	var wrap = $("#wrap-container"),
+		wrapHeight = wrap.height(),
+		car = $("#carousel-container"),
+		carHegiht = car.height(),
+		cwarpH = carHegiht - 51,
+		wrapList = $("#wrap-container li"),
+		dataWrapArr = wrapList.map(function(index, value){
+						return value.dataset['wrap'];
+					});
+		dataWrapHeightArr = dataWrapArr.map(function(i, v){
+			return $(v).offset().top;
+		});
+		wrapList.click(function(){
+			var height = dataWrapHeightArr[$(this).index()];
+			$("body, html").animate({
+				scrollTop: height - 50
+				},500, function() {
+				/* stuff to do after animation is complete */
+			});
+		});
+	$(window).scroll(function(event) {
+		/* Act on the event */
+		var scrollTop = $(window).scrollTop() ;
+		if(scrollTop > cwarpH){
+			wrap.addClass('wrap-fixed').removeClass('wrap-absolute');
+			console.log("fixed");
+		}
+		else{
+			wrap.addClass('wrap-absolute').removeClass('wrap-fixed');
+			console.log("absolute");
+		}
+
+		var qid = -1;
+
+		dataWrapHeightArr.map(function(index, value){
+			if(scrollTop >= value - 50){
+				qid = index;
+			}
+		});
+		if(qid != -1) $(wrapList[qid]).addClass('active').siblings().removeClass('active');
+		else wrapList.removeClass('active');
+
+	});
+});
