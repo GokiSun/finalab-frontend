b23ebd9f01b2d2d0c46085d7e986782087f8146e
WayneWang <1070546617@qq.com>
2018-08-04 21:16:34 +0800

    change some pages like:nav

---
+++ scripts/common.js
@@ -0,0 +1,41 @@
+
+$('.r-nav').html('<div class="nav-menu-content">\n' +
+    '\t\t\t\t\t<ul class="nav-menu">\n' +
+    '\t\t\t\t\t\t<li class="nav-header-item">\n' +
+    '\t\t\t\t\t\t\t<div class="header-item-detail"><a href="index.html">首页</a><span class="fixed-bottom"></span></div>\n' +
+    '\t\t\t\t\t\t</li>\n' +
+    '\t\t\t\t\t\t<li class="nav-header-item">\n' +
+    '\t\t\t\t\t\t\t<div class="header-item-detail"><a href="service.html">项目</a><span class="fixed-bottom"></span></div>\n' +
+    '\t\t\t\t\t\t</li>\n' +
+    '\t\t\t\t\t\t<li class="nav-header-item">\n' +
+    '\t\t\t\t\t\t\t<!--<div class="header-item-detail"><a href="javascript:void(0)">产品接口</a><span class="fixed-bottom"></span></div>-->\n' +
+    '\t\t\t\t\t\t</li>\n' +
+    '\t\t\t\t\t\t<!--<li class="nav-header-item">-->\n' +
+    '\t\t\t\t\t\t\t<!--<div class="header-item-detail"><a href="javascript:void(0)">项目</a><span class="fixed-bottom"></span></div>-->\n' +
+    '\t\t\t\t\t\t<!--</li>-->\n' +
+    '\t\t\t\t\t\t<li class="nav-header-item">\n' +
+    '\t\t\t\t\t\t\t<div class="header-item-detail"><a href="about.html">关于凡路</a><span class="fixed-bottom"></span></div>\n' +
+    '\t\t\t\t\t\t</li>\n' +
+    '\t\t\t\t\t</ul>\n' +
+    '\t\t\t\t\t<div class="nav-login"><a class="login-f"><i class="login-i icon iconfont icon-modaologin"></i><span>登录</span></a></div>\n' +
+    '\t\t\t\t</div>')
+
+$('#footer-bar').html('<div class="footer-up">\n' +
+    '\t\t\t\t<div class="footer-up-l"><img src="images/logo.png" alt="logo"></div>\n' +
+    '\t\t\t\t<div class="footer-up-r">\n' +
+    '\t\t\t\t\t<a href="javascript:void(0)">友情链接：</a>\n' +
+    '\t\t\t\t\t<a href="javascript:void(0)">长沙理工大学</a>\n' +
+    '\t\t\t\t\t<a href="javascript:void(0)">长沙理工大学计通学院</a>\n' +
+    '\t\t\t\t\t<a href="javascript:void(0)">大学生科技交流平台</a>\n' +
+    '\t\t\t\t\t<a href="javascript:void(0)">长沙理工大学创业园</a>\n' +
+    '\t\t\t\t</div>\n' +
+    '\t\t\t</div>\n' +
+    '\t\t\t<div class="footer-bot">\n' +
+    '\t\t\t\t<div class="footer-bot-l">\n' +
+    '\t\t\t\t\t<img src="images/zhaoxin.png" alt="link1">\n' +
+    '\t\t\t\t</div>\n' +
+    '\t\t\t\t<div class="footer-bot-r">\n' +
+    '\t\t\t\t\t<p>Copyright©2016 凡路团队 版权所有 </p>\n' +
+    '\t\t\t\t\t<p>地址：湖南省长沙市雨花区万家丽南路2段960号</p>\n' +
+    '\t\t\t\t</div>\n' +
+    '\t\t\t</div>')
